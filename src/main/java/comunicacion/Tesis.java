8e1f98bf7317b54a80f74af529bd507f5c8ffa95
mateocs22 <mcarvajalsa@unal.edu.co>
2021-12-07 20:53:58 -0500

    Sol2

--- src/main/java/comunicacion/Tesis.java
+++ src/main/java/comunicacion/Tesis.java
@@ -20,12 +20,12 @@ public class Tesis extends Escrito {
 	
 
 	@Override
-	int palabrasTotales(int palabrasPagina) {
+	public int palabrasTotales(int palabrasPagina) {
 		return this.getPaginas() * 5;
 	}
 
 	@Override
-	String interpretacion() {
+	public String interpretacion() {
 		return interpretacion;
 	}
 
